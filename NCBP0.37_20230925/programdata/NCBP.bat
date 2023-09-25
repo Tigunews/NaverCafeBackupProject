3ae6c8a6b65cbf0876c53866f1bdbba8bf74b735
Tigunews <gkr0325@hanmail.net>
2023-09-25 12:11:12 +0900

    202309251211

---
+++ NCBP0.37_20230925/programdata/NCBP.bat
@@ -0,0 +1,6 @@
+ECHO OFF
+ECHO 실행중...
+start C%USERPROFILE%\Miniconda3\pkgs\python-3.7.0-hea74fb7_0\Scripts\idle.exe -r C:\Users\%username%\NCBP\programdata\NCBP.py
+ECHO 잠시후 이 창은 자동으로 닫힙니다.
+timeout 3
+exit
